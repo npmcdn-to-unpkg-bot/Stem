54f2ad5e9a8d527dd103dd8e785413b971423e16
Davith <emaildavith@gmail.com>
2016-06-21 23:00:12 -0700

    merging to local master

--- assets/jsx/artist-components/artist-internal.jsx
+++ assets/jsx/artist-components/artist-internal.jsx
@@ -4,6 +4,9 @@ var ArtistInternal = React.createClass({
     return (
       <div className="artist-internal-wrapper">
         <ArtistInternalSideBar />
+        <div className="artist-internal-greeting col-lg-9">
+          <h3>Hello, Band Name!</h3>
+        </div>
         <ArtistUpload />
       </div>
     )
--- assets/jsx/artist-components/artist-upload.jsx
+++ assets/jsx/artist-components/artist-upload.jsx
@@ -3,18 +3,34 @@ var ArtistUpload = React.createClass({
     var self = this;
     return (
       <div className="artist-upload-wrapper col-lg-9">
-        <h3><span className="glyphicon glyphicon-headphones"></span> Upload your work <span className="glyphicon glyphicon-chevron-up pull-right"></span></h3>
+        <h3><span className="glyphicon glyphicon-headphones"></span> Upload your work <span className="glyphicon glyphicon-chevron-up artist-upload-up pull-right"></span></h3>
         <div className="artist-upload-drop col-lg-12">
           <h4><span className="glyphicon glyphicon-folder-close"></span> Drag and drop audio files or <a>Browse</a> your computer</h4>
         </div>
-        <div className="col-lg-12">
+        <div className="artist-upload col-lg-12">
           <div className="col-lg-6">
             <p>Artist Name</p>
             <input type="" className="artist-upload-input col-lg-12" />
+            <a><span className="artist-add-artist-link glyphicon glyphicon-plus-sign"></span> Add Additional Artists</a>
           </div> 
           <div className="col-lg-6">
             <p>Add Additional Artist</p>
             <input type="" className="artist-upload-input col-lg-12" />
+          </div>
+          <div className="col-lg-12">
+            <p>Album Name</p>
+            <input type="" className="artist-upload-input col-lg-12" />
+          </div>
+          <div className="col-lg-12">
+            <p>Promotion Link</p>
+            <input type="" className="artist-upload-input col-lg-12" />
+          </div>
+          <div className="col-lg-12">
+            <p>Add Tags</p>
+            <input type="" className="artist-upload-input col-lg-12" />
+          </div>
+          <div className="col-lg-12">
+            <button type="submit" className="artist-upload-btn btn col-lg-12"><h4><span className='glyphicon glyphicon-user'></span> Upload</h4></button>
           </div>
         </div>  
       </div>
