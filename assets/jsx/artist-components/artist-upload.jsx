3402da46219d1968fe1508cd79a591824e11a23d
Davith <emaildavith@gmail.com>
2016-06-21 21:01:45 -0700

    pulling to sync

--- assets/jsx/artist-components/artist-upload.jsx
+++ assets/jsx/artist-components/artist-upload.jsx
@@ -3,10 +3,20 @@ var ArtistUpload = React.createClass({
     var self = this;
     return (
       <div className="artist-upload-wrapper col-lg-9">
-        <h3><span className="glyphicon glyphicon-headphones"></span> Upload your work</h3>
+        <h3><span className="glyphicon glyphicon-headphones"></span> Upload your work <span className="glyphicon glyphicon-chevron-up pull-right"></span></h3>
         <div className="artist-upload-drop col-lg-12">
           <h4><span className="glyphicon glyphicon-folder-close"></span> Drag and drop audio files or <a>Browse</a> your computer</h4>
         </div>
+        <div className="col-lg-12">
+          <div className="col-lg-6">
+            <p>Artist Name</p>
+            <input type="" className="artist-upload-input col-lg-12" />
+          </div> 
+          <div className="col-lg-6">
+            <p>Add Additional Artist</p>
+            <input type="" className="artist-upload-input col-lg-12" />
+          </div>
+        </div>  
       </div>
     )
   }
