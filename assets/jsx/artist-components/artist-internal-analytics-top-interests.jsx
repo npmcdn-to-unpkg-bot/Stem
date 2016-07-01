369f3399e3917ba1b8e81fb721a4637ae03e80b8
Davith <emaildavith@gmail.com>
2016-06-30 21:48:10 -0700

    syncing

--- assets/jsx/artist-components/artist-internal-analytics-top-interests.jsx
+++ assets/jsx/artist-components/artist-internal-analytics-top-interests.jsx
@@ -1,50 +1,50 @@
 var ArtistInternalAnalyticsTopInterests = React.createClass({
   render: function () {
     return(
-      <div className="artist-internal-top-interest-wrapper col-lg-12">
+      <div className="artist-internal-top-interest-wrapper ">
         <div className="col-lg-12">
           <h3 className="col-lg-3">Top Interest</h3>
           <h4 className="col-lg-3 pull-right"><span className="glyphicon glyphicon-eye-open"></span>View All</h4>
         </div>
         <div className="col-lg-12">
-          <ul className="artist-internal-top-interest-list col-lg-12">
+          <ul className="artist-internal-analytics-top-interest-list col-lg-12">
             <li className="">
-              <img className="artist-internal-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
+              <img className="artist-internal-analytics-top-interest-img " src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
+              <div className="artist-internal-top-interest-detail ">
+                <h5>Melaina Bell</h5>
+                <a>Collaborate</a>
+              </div>  
+            </li>
+            <li className="">
+              <img className="artist-internal-analytics-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
               <div className="artist-internal-top-interest-detail">
                 <h5>Melaina Bell</h5>
                 <a>Collaborate</a>
               </div>  
             </li>
             <li className="">
-              <img className="artist-internal-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
+              <img className="artist-internal-analytics-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
               <div className="artist-internal-top-interest-detail">
                 <h5>Melaina Bell</h5>
                 <a>Collaborate</a>
               </div>  
             </li>
             <li className="">
-              <img className="artist-internal-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
+              <img className="artist-internal-analytics-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
               <div className="artist-internal-top-interest-detail">
                 <h5>Melaina Bell</h5>
                 <a>Collaborate</a>
               </div>  
             </li>
             <li className="">
-              <img className="artist-internal-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
+              <img className="artist-internal-analytics-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
               <div className="artist-internal-top-interest-detail">
                 <h5>Melaina Bell</h5>
                 <a>Collaborate</a>
               </div>  
             </li>
             <li className="">
-              <img className="artist-internal-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
-              <div className="artist-internal-top-interest-detail">
-                <h5>Melaina Bell</h5>
-                <a>Collaborate</a>
-              </div>  
-            </li>
-            <li className="">
-              <img className="artist-internal-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
+              <img className="artist-internal-analytics-top-interest-img" src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
               <div className="artist-internal-top-interest-detail">
                 <h5>Melaina Bell</h5>
                 <a>Collaborate</a>
