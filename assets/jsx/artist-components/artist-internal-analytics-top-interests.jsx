6720dcc309131e782e4f62e1e19651ab3e23ac6a
Davith <emaildavith@gmail.com>
2016-07-06 19:58:22 -0700

    syncing merged code

--- assets/jsx/artist-components/artist-internal-analytics-top-interests.jsx
+++ assets/jsx/artist-components/artist-internal-analytics-top-interests.jsx
@@ -1,4 +1,22 @@
 var ArtistInternalAnalyticsTopInterests = React.createClass({
+  getInitialState: function() {
+    return { hovered: false }
+  },
+  hoverState: function() {
+    if (this.state.hovered) {
+      return { color: 'red' }
+    } else {
+      return { color: "blue" }
+    }
+  },
+
+  onMouseOver : function () {
+    this.setState({ hovered:true });
+  },
+
+  onMouseOut : function () {
+    this.setState({ hovered:false });
+  },
   render: function () {
     return(
       <div className="artist-internal-top-interest-wrapper ">
@@ -8,8 +26,8 @@ var ArtistInternalAnalyticsTopInterests = React.createClass({
         </div>
         <div className="col-lg-12">
           <ul className="artist-internal-analytics-top-interest-list col-lg-12">
-            <li className="">
-              <img className="artist-internal-analytics-top-interest-img " src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" />
+            <li className=""  >
+              <a ><img className="artist-internal-analytics-top-interest-img "  src="https://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s640x640/sh0.08/e35/11375351_691728300859406_959765053_n.jpg" /></a>
               <div className="artist-internal-top-interest-detail ">
                 <h5>Melaina Bell</h5>
                 <a>Collaborate</a>
