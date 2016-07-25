255d283daf72f31a9c16440cf6b573f703e8c9ac
Davith <emaildavith@gmail.com>
2016-07-25 16:14:25 -0700

    updated library views

--- assets/jsx/artist/internalProfile/library.jsx
+++ assets/jsx/artist/internalProfile/library.jsx
@@ -42,27 +42,27 @@ var ArtistInternalLibraryTable = React.createClass({
   render: function () {
     var self = this;
     return (
-        <div className='artist-internal-library-wrapper  no-gutters col-lg-12'>
-          <div className="library-approval-list pad-b-lg">
-            <ul className="col-sm-12">
+        <div className='artist-internal-library-wrapper col-lg-12'>
+          <div className="library-approval-list row no-gutters pad-b-lg">
+            <ul className="row no-gutters col-sm-12">
               <li className="col-sm-3"><h4><span className={this.state.underlineApproved ? "library-list-underline" : null} onClick={this.handleApproved}>Approved</span></h4></li>
               <li className="col-sm-3"><h4><span className={this.state.underlinePending ? "library-list-underline" : null} onClick={this.handlePending}>Pending</span></h4></li>
               <li className="col-sm-3"><h4><span className={this.state.underlineDisabled ? "library-list-underline" : null} onClick={this.handleDisabled}>Disabled</span></h4></li>
               <li className="col-sm-3"><h4><span className={this.state.underlineRejected ? "library-list-underline" : null} onClick={this.handleRejected}>Rejected</span></h4></li>
             </ul>
           </div>
-          <div className={this.state.underlineApproved ? "display-true" : "display-false"}>
-            <LibraryApproved />
-          </div>
-          <div className={this.state.underlinePending ? "display-true" : "display-false"}>
-            <LibraryPending />
-          </div>
-          <div className={this.state.underlineDisabled ? "display-true" : "display-false"}>
-            <LibraryDisabled />
-          </div>
-          <div className={this.state.underlineRejected ? "display-true" : "display-false"}>
-            <LibraryRejected />
-          </div>
+            <div className={this.state.underlineApproved ? "display-true" : "display-false"}>
+              <LibraryApproved />
+            </div>
+            <div className={this.state.underlinePending ? "display-true" : "display-false"}>
+              <LibraryPending />
+            </div>
+            <div className={this.state.underlineDisabled ? "display-true" : "display-false"}>
+              <LibraryDisabled />
+            </div>
+            <div className={this.state.underlineRejected ? "display-true" : "display-false"}>
+              <LibraryRejected />
+            </div>
         </div>  
     )
   }
