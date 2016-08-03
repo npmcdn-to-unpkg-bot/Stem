780f12986a634021c9bbe5d7c6bb2121cbe38d24
Rylin Zbitowsky <rylinzbitowsky@gmail.com>
2016-08-02 20:48:32 -0700

    Add ArtistTaggedSuccess styling

--- assets/scripts/artist/settings/settingsMain.jsx
+++ assets/scripts/artist/settings/settingsMain.jsx
@@ -4,19 +4,17 @@ var ArtistAccountSettings = React.createClass({
 
     return (
       <div className="row no-gutters ">
-<<<<<<< HEAD
         <div className="artist-account-navbar-wrapper col-sm-2">
             <ArtistAccountNavBar />
         </div>
         <div className="artist-account-wrapper col-xs-12 col-sm-9">
           <ArtistAccountView />
-=======
+        </div>
         <div className="col-sm-2">
             <ArtistAccountNavBar />
         </div>
         <div className="artist-account-wrapper col-xs-12 col-sm-10">
           <ArtistAccountSettings />
->>>>>>> 68435701d0f938975a8b3aeeb3de1bf29e0b3a2c
           <ArtistContactInfo />
           <ArtistConnectedAccounts />
           <ArtistNotification />
