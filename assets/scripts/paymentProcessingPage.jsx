118d00493d385d3a62bc880ffc3423ad9430bc1f
Rylin Zbitowsky <rylinzbitowsky@gmail.com>
2016-08-17 15:31:57 -0700

    Create Payment Process Page

---
+++ assets/scripts/paymentProcessingPage.jsx
@@ -0,0 +1,18 @@
+var PaymentProcessingPage = React.createClass({
+	render: function() {
+		return(
+			<div className="payment-processing-page pad-box-lg">
+				<h2>You're so close to having all this music at your finger tips!</h2>
+				<p>We're so stoked to have you join our family!</p>
+				<div className="payment-processing-info">
+					<div className="payment-personal-info">
+					<span className="circled-number">1</span>
+					</div>
+					<div className="payment-payment-details">
+					<span className="circled-number">2</span>
+					</div>
+				</div>
+			</div>
+		)
+	}
+})
