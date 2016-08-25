d5d8d910604e0cc29e4dae2798ff53d2ab97a9e9
Jeremy Miller <jjm340@gmail.com>
2016-08-25 02:00:36 -0700

    Added jQuery cropper.  Still need to fix overlapping div issue

--- assets/scripts/imageUpload/ImageUpload.jsx
+++ assets/scripts/imageUpload/ImageUpload.jsx
@@ -6,29 +6,60 @@ var ImageUpload = React.createClass({
 			id: 'imageUpload-' + lastImageUploadId++,
 			imageUrl: ''
 		};
-	},
-	componentDidMount: function() {
-
-	},
+	}, 
 	getId: function() {
 		return this.props.id ? this.props.id : this.state.id;
 	},
 	handleChangeFile: function(ev) {
-		// var fileReader = new FileReader(),
-		// 	file = ev.target.files[0];
-
-		// fileReader.onloadend = function() {
-		// 	this.setState({
-		// 		imageUrl: fileReader.result,
-		// 	});
-		// }.bind(this);
-
-		// fileReader.readAsDataURL(file);
+		var fileReader = new FileReader(),
+			file = ev.target.files[0];
+			//fileUrl = fileReader.readAsDataUrl(file);
+				//URL.createObjectURL(file);
+		
+		fileReader.readAsDataUrl(file);
+		fileReader.onloadend = function() {
+			var cropperEl = $('#' + this.getId());
+			cropperEl.cropper('reset').cropper('replace', fileReader.result);
+		}.bind(this);
 	},
 	render: function() {
+		var imageStyles = {
+			maxWidth: '100%'
+		};
+
+		var uploadDivStyles = {
+			marginBottom: '120px',
+			paddingBottom: '120px',
+			zIndex: 120
+		};
+
 		return (
-			<div id={this.getId()}>
+		<div className="album-art-box col-xs-12">
+			<div className="upload lg text-center">
+				{this.state.imageUrl ? 
+					<div className="upload-label update">
+						<h3>Upload your album art</h3>
+						<p>Recommended size: 1000x1000</p>
+						<p>Full Size Limit: xMB</p>
+					</div>
+				:
+					<div className="upload-label">
+						<h3>Upload your album art</h3>
+						<p>Recommended size: 1000x1000</p>
+						<p>Full Size Limit: xMB</p>
+					</div>
+				}
+				<img id={this.getId()} style={imageStyles} />
 			</div>
+			<div style={uploadDivStyles}>
+				<input 
+					onChange={this.handleChangeFile} 
+					type="file" />
+			</div>
+		</div>
+
+
+
 		);
 	}
 });
