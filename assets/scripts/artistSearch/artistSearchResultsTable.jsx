68ab20dd5ce3d549e048c27a8a777fbb1d03e87f
Rylin Zbitowsky <rylinzbitowsky@gmail.com>
2016-08-24 19:24:16 -0700

    Create Artist Search No Results pop up

--- assets/scripts/artistSearch/artistSearchResultsTable.jsx
+++ assets/scripts/artistSearch/artistSearchResultsTable.jsx
@@ -1,7 +1,7 @@
 var ArtistSearchResultsTable = React.createClass({
 	render: function() {
 	return (
-		<div className="col-xs-8 col-sm-9">
+		<div className="artist-search-table-wrapper mar-t-lg">
 	    	<table className="table col-xs-12 col-md-12 col-lg-12">
 	        	<ArtistSearchResultsTableHeader />
 	        	<tbody>
